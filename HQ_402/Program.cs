5f188dd714cbebeb45ef1b58e77e4306a2b33e9b
Ikuz0_tich <Kuznetsovilya469@yandex.ru>
2022-09-01 14:56:55 +0300

    задание 402

---
+++ HQ_402/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+
+int SumNum(int number)
+{
+
+    int sum = 0;
+    while (number > 0)
+    {
+        sum = sum + number % 10;
+        number = number / 10;
+        //Console.WriteLine(sum);
+    }
+    return sum;
+}
+
+Console.WriteLine(SumNum(number));
