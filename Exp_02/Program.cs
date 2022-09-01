3ad504d29aa5cc6085f8f7dcd6c69518195e543e
Ikuz0_tich <Kuznetsovilya469@yandex.ru>
2022-09-01 11:37:53 +0300

    02 решено

---
+++ Exp_02/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+int polindrom(int number)
+{
+
+    int pol = 0;
+    while (number > 0)
+    {
+        pol = pol * 10 + number % 10;
+        number = number / 10;
+        // Console.WriteLine(pol);
+    }
+    return pol;
+}
+
+int polEnd = polindrom(number);
+
+if (number == polEnd)
+{
+    Console.Write($"{number} -> Да");
+}
+else
+{
+    Console.Write($"{number} -> Нет");
+}
