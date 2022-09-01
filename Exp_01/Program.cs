60d8de4b9750870c9bb1fd37c25aa04bc819d464
Ikuz0_tich <Kuznetsovilya469@yandex.ru>
2022-09-01 11:07:29 +0300

    удалены коментарии

--- Exp_01/Program.cs
+++ Exp_01/Program.cs
@@ -1,8 +1,4 @@
-﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-3 -> 1, 8, 27
-5 -> 1, 8, 27, 64, 125 */
-
-Console.WriteLine("Введите число");
+﻿Console.WriteLine("Введите число");
 int number = int.Parse(Console.ReadLine()!);
 Console.Write($"{number} ->");
 for (int i = 1; i < number + 1; i++)
